c38c50a92b3ba497e9908d2ae125d4177ae15dae
Andrei Vayavoda <andreivoe@gmail.com>
2017-10-26 19:12:02 +0200

    builded project with rtc_base support

--- Horoscopes/Horoscopes/ViewController.mm
+++ Horoscopes/Horoscopes/ViewController.mm
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "rtc_base/logging.h"
 
 @interface ViewController ()
 
@@ -16,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    LOG(LS_ERROR) << "!";
     // Do any additional setup after loading the view, typically from a nib.
 }
 

c1b24fdf06250bfe0a39e351bb6fd6096f38337e
Andrei Vayavoda <andreivoe@gmail.com>
2017-11-03 06:28:20 +0100

    .

--- server/datastoreupdate.js
+++ server/datastoreupdate.js
@@ -1,6 +1,59 @@
-
+const Firestore = require('@google-cloud/firestore');
+credentialsFilename = "./horo-ios-287dcbc8f4c6.json"
 const Browser = require('zombie');
 var gumbo = require("gumbo-parser");
+
+var zodiacs = [
+"aquarius",
+"pisces",
+"aries",
+"taurus",
+"gemini",
+"cancer",
+"leo",
+"virgo",
+"libra",
+"scorpio",
+"sagittarius",
+               "capricorn"];
+
+const firestore = new Firestore({projectId: 'horo-ios',
+                                keyFilename: credentialsFilename });
+const document = firestore.doc('posts/intro-to-firestore');
+// Enter new data into the document.
+/*
+document.set({
+             title: 'Welcome to Firestore',
+             body: 'Hello World',
+             }).then(() => {
+                     console.log('created!');
+                     
+                     // Update an existing document.
+                     document.update({
+                                     body: 'My first Firestore app',
+                                     }).then(() => {
+                                             console.log('updated!');
+                                             // Document updated successfully.
+                                             
+                                             // Read the document.
+                                             document.get().then(doc => {
+                                                                 console.log('Read!');
+                                                                 // Document read successfully.
+                                                                 
+ 
+                                                                 // Delete the document.
+                                                          //       document.delete().then(() => {
+                                                          //                              console.log('Delete!');
+                                                          //                              // Document deleted successfully.
+                                                           //                             });
+ 
+                                                                 });
+                                             
+                                             });
+                     
+
+                     });
+*/
 
 // We're going to make requests to http://example.com/signup
 // Which will be routed to our test server localhost:3000
@@ -16,7 +69,6 @@ Browser.extend(function(browser) {
                           });
                });
 
-const browser = new Browser();
 console.log('hello1')
 /*
 browser.visit('/', function() {
@@ -77,15 +129,15 @@ function invokeTextFromTreeWithZodiacKey(tree, key) {
     }
 }
 
-function invokePayloadData(tree) {
+function invokePayloadData(tree, completion) {
     if (tree.nodeName == "div") {
         for (var y = 0; y < tree.attributes.length; y++) {
             attribute = tree.attributes[y];
             if (attribute.name == "class" && attribute.value == "horoscope-content") {
-                console.log('found payload tree: ');
+               // console.log('found payload tree: ');
                 textStorage = ""
                 invokeTextFromTreeWithZodiacKey(tree, zodiacKey)
-                console.log( "" + zodiacKey + ': textStorage: ' + textStorage)
+               // console.log( "" + zodiacKey + ': textStorage: ' + textStorage)
             }
         }
     }
@@ -97,25 +149,59 @@ function invokePayloadData(tree) {
             }
         }
     }
+    
     for (var i = 0; i < tree.childNodes.length; i++) {
         var child = tree.childNodes[i];
         if (child.nodeType == 1 /*1 == Element*/) {
-            invokePayloadData(child)
+            invokePayloadData(child, null)
         }
+    }
+    if (completion != null) {
+        completion(zodiacKey, textStorage)
     }
 }
 
-browser.fetch("https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=3")//"https://horoscope.com")
-.then(function(response) {
-      console.log('Status code:', response.status);
-      if (response.status === 200)
-      return response.text();
-      })
-.then(function(text) {
-      var tree = gumbo(text);
-      var root = tree["root"]
-      invokePayloadData(root)
-      })
-.catch(function(error) {
-       console.log('Network error');
-       });
+function doRequest(signIndex, completion) {
+    var url = "https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=" + signIndex
+    console.log("start requesting: " + url)
+    const browser = new Browser()
+    browser.fetch(url)
+    .then(function(response) {
+          console.log('Status code:', response.status);
+          if (response.status === 200)
+          return response.text();
+          })
+    .then(function(text) {
+          var tree = gumbo(text);
+          var root = tree["root"]
+          invokePayloadData(root, function (zodiacName, predictionText) {
+                            console.log('zodiacName: ' + zodiacName + '; predictionText: ' + predictionText)
+                            })
+          if (completion) {
+            completion(true)
+          }
+          })
+    .catch(function(error) {
+           console.log("parsing html error!");
+           console.log(error);
+           if (completion) {
+             completion(false)
+           }
+           });
+}
+
+function requestHoroscopesWithSignsArray(array) {
+    if (array.length == 0) {
+        console.log("end of requests horoscopes. Array empty");
+        return;
+    }
+    index = array[0]
+    array.splice(0, 1);
+    doRequest(index, function (success) {
+              console.log("parsing success: " + success)
+                requestHoroscopesWithSignsArray(array)
+              })
+}
+
+indexes = [0]
+requestHoroscopesWithSignsArray(indexes)

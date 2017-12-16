656fe92a5c2f81c92df67cc56cdd2bfed1af3bfb
Andrei Vayavoda <andreivoe@gmail.com>
2017-12-16 15:20:14 +0100

    1) обновления в datastoreupdate.js - подготовка к выкатке в Firestore

--- server/datastoreupdate.js
+++ server/datastoreupdate.js
@@ -1,10 +1,24 @@
 const Firestore = require('@google-cloud/firestore');
 credentialsFilename = "./horo-ios-287dcbc8f4c6.json"
+credentialsJson = {
+    "type": "service_account",
+    "project_id": "horo-ios",
+    "private_key_id": "287dcbc8f4c6c2b20f780cafe7e2d41fc6e4db86",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/Tzu5JlkwEkQP\nVMYQxazgcngAwFAGOUCgio1Copm453o5J0WAoxl1OLr0wDtQb1hYAJhFHH5oyT2R\nmcwDO9c3/yLwkNtF8i2SAVUMRGkjap/js6n9op2t1UdW5NfnuR+lBB7sC3BinppY\n4GsH5J3f9ME7ubUVM/k1ywFUM3T8lWEvfAhQ2Iz8kC0IHMpIOB0BIhF3+ixUJkJi\nOeeVVS8URTh8cbFfo2LEYVGj8ygJb+gZ//0bKUxkuz7BYePARYwyhjF+9loGzJsg\ngU+qucdLWORJcv/D5T/1isiBcLy9drY+DAjcZESlpJHA7qD2DwVZAyaQxeXDOsM1\n3xQuF0otAgMBAAECggEAQv8VdD6oxzvuV8whXOhNJYEQ43p1y+gq6M38sFRPL0Dp\nPbCyF12G4dVPK71SSFXmA1OZ/8H9xuaKyD2rA4rmUPbpnoOsNq+cI+CRjy06AKwr\neEuIAYn8XE8vn4+eyMi8+0YZKiLLZc4TUYKuGOKII1EMhsT/VICSCrOgMIOguX9e\nvarg8jGNY88uS0i1QvGsbzh2sZgyknmnzO692LEBKyqIF1nYpR0ToUcfxPMJ61qk\nzj+4cjaeqfN/9fYtYmAESsWQD+mPxUJpNH3/sKr2X4Cv3ubAPBgdkHteIXVGEiM/\nsu5kRrsgHxZCXEBCNFsJpfMin0/u90itrm92m2P0BQKBgQD3zmjaDvGxpkN+tpHh\nAUEjRlw51GMbkD3+2iIJZAsGpCgiZr12i2xg+8KKr6gTpDN4i3irXRYhdXdE+/QF\nsFQvLTeUeap0KstGH0xd4ZkT+WyKMjvYM83rai+odYnnTFHRBxNRwKqcOJe9fmHo\nWza58LmszmP77YEM3zn5OAQ7/wKBgQDFoplTTLoZga7ZDmTyigCQW2fTFmPr8BBw\nKgYaDWLPb8siLV+5w6G1/KhE1B2VSfrnL80gLIi+2BXU7Hp9eM9kMfs0QVJIGK6Y\nn17+Z58iAlODT16x1P9z8qq50fky5f3vgg+DGybhp+JfC8O3pmAp7wsVdK0W8KT8\n9y+KIwIp0wKBgDOWIZe7Yq8zyHs5cHbhtvir7gkP4K3dQjqqmElSLUVtqPk4YBqs\nfZxW7LHITXoBRcHDbxRlZXUDqhePR8ZjZbMIe7iJ92UFf5mSbGhizWYuF11RhcnF\nRwnJ6i/sgA/JgIK+MVRkgl/RHC2TgLhJPMRKi6ygronEcIgIdF4gPilrAoGAFvhT\n+7OCOa6x5LZgkzPo5t5IzebfTC/FqVOpC+QcZOGoaVt+sbeEFwO7huxkV9KWX8JV\ng/TJG+0/dFTb07Nz30BT4vnol0qPGFwFjWn7u9crX6qc5exMmGxO3XQDE6BZiDnE\nlcdVGIsLq2JRqAmlSZpn3mGGMJFdi60rEFILUVMCgYEAogETqbdU5XX69Y1i4mcb\nkToRZIHy4uRKTp4lHBvi0wMYtKCRIErRF8/HoemdwtER6+vHDYQaoGV5aefrJMi1\ncLAS9BtOwTXZIYUV3d+O9DDg8+/fRRSf+sSx8CVPYs5Ti1Jpg61fvDH2kf8lY7rs\nlXydv+6pgeG2LbEHQrvwgk4=\n-----END PRIVATE KEY-----\n",
+    "client_email": "redactor@horo-ios.iam.gserviceaccount.com",
+    "client_id": "113164697857028802069",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/redactor%40horo-ios.iam.gserviceaccount.com"
+}
+
+
 const Browser = require('zombie');
 var gumbo = require("gumbo-parser");
+var jsonfile = require('jsonfile')
 
-var zodiacs = [
-"aquarius",
+var zodiacs = ["aquarius",
 "pisces",
 "aries",
 "taurus",
@@ -15,50 +29,11 @@ var zodiacs = [
 "libra",
 "scorpio",
 "sagittarius",
-               "capricorn"];
+"capricorn"];
 
-const firestore = new Firestore({projectId: 'horo-ios',
-                                keyFilename: credentialsFilename });
-/*
-const document = firestore.doc('posts/intro-to-firestore');
-*/
- // Enter new data into the document.
-/*
-document.set({
-             title: 'Welcome to Firestore',
-             body: 'Hello World',
-             }).then(() => {
-                     console.log('created!');
-                     
-                     // Update an existing document.
-                     document.update({
-                                     body: 'My first Firestore app',
-                                     }).then(() => {
-                                             console.log('updated!');
-                                             // Document updated successfully.
-                                             
-                                             // Read the document.
-                                             document.get().then(doc => {
-                                                                 console.log('Read!');
-                                                                 // Document read successfully.
-                                                                 
- 
-                                                                 // Delete the document.
-                                                          //       document.delete().then(() => {
-                                                          //                              console.log('Delete!');
-                                                          //                              // Document deleted successfully.
-                                                           //                             });
- 
-                                                                 });
-                                             
-                                             });
-                     
+var newCredentialsFilename = './credentials.json'
+var firestore = null
 
-                     });
-*/
-
-// We're going to make requests to http://example.com/signup
-// Which will be routed to our test server localhost:3000
 Browser.localhost('mail.ru', 80);
 Browser.extend(function(browser) {
                browser.on('console', function(level, message) {
@@ -72,12 +47,6 @@ Browser.extend(function(browser) {
                });
 
 console.log('hello1')
-/*
-browser.visit('/', function() {
-              console.log(browser.location.href);
-              console.log(browser.title)
-              });
-*/
 
 var zodiacKey = ""
 var parameters = {}
@@ -310,15 +279,21 @@ function requestHoroscopesWithSignsArray(array, type, callback) {
     })
 }
 
-indexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-requestHoroscopesWithSignsArray(indexes.slice(), "yesterday", function () {
-  requestHoroscopesWithSignsArray(indexes.slice(), "today", function () {
-     requestHoroscopesWithSignsArray(indexes.slice(), "tomorrow", function () {
-       requestHoroscopesWithSignsArray(indexes.slice(), "weekly", function () {
-         requestHoroscopesWithSignsArray(indexes.slice(), "monthly", function () {
-            console.log("done!")
+jsonfile.writeFile(newCredentialsFilename, credentialsJson, function (err) {
+                   
+  console.error(err)
+  firestore = new Firestore({projectId: 'horo-ios', keyFilename: newCredentialsFilename });
+  indexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  requestHoroscopesWithSignsArray(indexes.slice(), "yesterday", function () {
+    requestHoroscopesWithSignsArray(indexes.slice(), "today", function () {
+       requestHoroscopesWithSignsArray(indexes.slice(), "tomorrow", function () {
+         requestHoroscopesWithSignsArray(indexes.slice(), "weekly", function () {
+           requestHoroscopesWithSignsArray(indexes.slice(), "monthly", function () {
+              console.log("done!")
+           })
          })
        })
      })
    })
- })
+
+});
